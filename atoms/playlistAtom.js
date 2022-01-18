1f271c578fb5638e56dbb4ac16092de798da04b3
aleksandraskrzypek <aleksandraskrzypek97@gmail.com>
2022-01-18 12:19:42 +0100

    spotify-clone

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import {atom} from "recoil";
+
+export const playlistState = atom({
+    key: "playlistState",
+    default: null,
+});
+
+export const playlistIdState = atom({
+    key: "playlistIdState",
+    default: '7HYnzkpNBNaujB9do8U3DL',
+});
